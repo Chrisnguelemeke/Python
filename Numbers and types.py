1fddae9ce2498b4c4b28122d8338911be203e5a1
Chrisnguelemeke <cnguelemeke@ivytech.edu>
2026-08-31 11:17:31 -0400

    Add files via upload

---
+++ Numbers and types.py
@@ -0,0 +1,12 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 60*60
+3600
+>>> seconds_per_hour = 60*60
+>>> seconds_per_hour*24
+86400
+>>> seconds_per_day = seconds_per_hour*24
+>>> seconds_per_day/seconds_per_hour
+24.0
+>>> seconds_per_day // seconds_per_hour
+24
